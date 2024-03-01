d8180768cbfad5636c68a6df218d7ee865ded8a2
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-02-29 19:29:52 -0700

    Created Button as well as styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,19 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
@@ -39,6 +50,7 @@ function Navbar() {
                             </NavLink>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn-secondary'>Sign Up</Button>}
                 </div>
             </nav>
         </>
