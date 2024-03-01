7f9b61bad7a4cd5fd9338328250127090ab35c23
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-03-01 14:10:09 -0700

    Added functionality for HeroSection and more CSS

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../App/App.css';
+import { Button } from '../Button/Button';
+import './HeroSection.css';
 
 function HeroSection() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='hero-container'>
+            <video src='/videos/video-1.mp4' autoPlay loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className='hero-btns'>
+                <Button className='btns' buttonStyle='btn-secondary' buttonSize='btn-large'>GET STARTED</Button>
+                <Button className='btns' buttonStyle='btn-primary' buttonSize='btn-large' onClick={console.log('hey')}>WATCH TRAILER
+                    <ion-icon name="play-circle-outline" /></Button>
+            </div>
+        </div>
+    );
 }
 
-export default HeroSection
+export default HeroSection;
