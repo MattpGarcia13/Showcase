c785a8aae50aaa86547385479f59da87ef052044
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-03-02 14:15:56 -0700

    Added more Routing, adjusted file setup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,9 @@ import Navbar from '../Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home'
 import PlanningHunt from '../Pages/PlanningHunt';
+import AboutUs from '../AboutUs/AboutUs';
+import SignUp from '../SignUp/SignUp';
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/planningHunt' element={<PlanningHunt />} /> 
+        <Route path='/aboutUs' element={<AboutUs />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
       <Footer />
     </div>
