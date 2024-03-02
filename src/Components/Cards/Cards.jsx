80bad9489d1130b8522bb5b15cfa9d28139d2438
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-03-02 12:30:05 -0700

    Created AboutUs page, made adjustments

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Cards.css';
-import CardItem from '../Cards/CardItem'
+import CardItem from '../Cards/CardItem';
+import { useEffect } from 'react';
 
 function Cards({ dinos }) {
+    const [filteredDinos, setFilteredDinos] = useState([]);
+
+    useEffect(() => {
+        setFilteredDinos([...dinos]);
+    }, [dinos]);
+
     function sortByHerbivores() {
-        const dinoArray = [...dinos]
-        dinoArray.filter(dino => dino.diet === 'Herbivore')
+        const herbivores = dinos.filter((dino) => dino.diet === 'Herbivore');
+        setFilteredDinos(herbivores);
     }
+
     function sortByCarnivores() {
-        const dinoArray = [...dinos]
-        dinoArray.filter(dino => dino.diet === 'Carnivore')
+        const carnivores = dinos.filter((dino) => dino.diet === 'Carnivore');
+        setFilteredDinos(carnivores);
     }
+
     function sortByRandom() {
-        let randomIndex = Math.floor(Math.random() * dinos.length)
-        let randomDino = dinos[randomIndex]
+        let randomIndex = Math.floor(Math.random() * dinos.length);
+        let randomDino = dinos[randomIndex];
+        setFilteredDinos([randomDino]);
     }
-    const dinoCards = dinos.map((dino) => {
+
+    const dinoCards = filteredDinos.map((dino) => {
         return (
             <CardItem
                 key={dino.name}
@@ -25,20 +36,25 @@ function Cards({ dinos }) {
                 name={dino.name}
                 img={dino.img}
             />
-        )
-    })
+        );
+    });
+
     return (
         <div className='cards'>
             <div className='sortButtons-container'>
-                <button onClick={sortByHerbivores} className='sort-cards'>Sort by Herbivores!</button>
-                <button onClick={sortByCarnivores} className='sort-cards'>Sort by Carnivores!</button>
-                <button onClick={sortByRandom} className='sort-cards'>Random!</button>
+                <button onClick={sortByHerbivores} className='sort-cards'>
+                    Sort by Herbivores!
+                </button>
+                <button onClick={sortByCarnivores} className='sort-cards'>
+                    Sort by Carnivores!
+                </button>
+                <button onClick={sortByRandom} className='sort-cards'>
+                    Random!
+                </button>
             </div>
             <h1>Have a look at over 30 of the most popular mesozoic creatures!</h1>
             <div className='cards_container'>
-                <div className='cards_wrapper'>
-                    {dinoCards}
-                </div>
+                <div className='cards_wrapper'>{dinoCards}</div>
             </div>
         </div>
     );
--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -6,14 +6,11 @@ import './HeroSection.css';
 const HeroSection = () => {
   return (
     <div className='hero-container'>
-      <video src='/Videos/mod3finalvideo1.mp4' autoPlay loop muted />
+      <video src='/Videos/showcaseMod3dino.mp4' autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn-secondary' buttonSize='btn-large'>GET STARTED</Button>
-        <Button className='btns' buttonStyle='btn-primary' buttonSize='btn-large' onClick={() => console.log('hey')}>
-          WATCH TRAILER <ion-icon name="play-circle-outline" />
-        </Button>
       </div>
     </div>
   );
